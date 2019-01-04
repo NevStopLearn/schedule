4011746fca5145156e109e84bf60fc2607f86789
JiYang <2574714005@qq.com>
2019-01-04 18:16:41 +0800

    schedule study

---
+++ app/Console/Kernel.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console;
+
+use App\Console\Commands\SayHello;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        //SayHello::class,
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        // $schedule->command('inspire')
+        //          ->hourly();
+         $schedule->command('message:hello')
+                  ->everyMinute();
+         $schedule->call(function(){
+             Log::info('我是call()方法调用的定时任务');
+         })->everyMinute();
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
